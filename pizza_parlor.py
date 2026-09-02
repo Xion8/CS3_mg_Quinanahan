5a3b769cb2b8f5f39628cc6acaaf3112526df85e
Xion8 <quinanahanxn30@wvc.pshs.edu.ph>
2026-09-02 10:20:31 +0900

    Add files via upload

---
+++ pizza_parlor.py
@@ -0,0 +1,20 @@
+class Pizza_Parlor:
+
+
+    def __init__(self, mushrooms, extracheese, pepperoni, Final_Price, topping_count):
+        self.mushrooms = mushrooms
+        self.extracheese = extracheese
+        self.pepperoni = pepperoni
+        self.Final_Price = Final_Price
+
+
+
+        def display_info(self):
+
+
+
+        def Final_Price (self):
+        ("total_toppings")*1.50 + 10.00 = self.Final_Price
+        return self.Final_Price
+
+    
